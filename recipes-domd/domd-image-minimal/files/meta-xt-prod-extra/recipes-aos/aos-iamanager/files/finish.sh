60a1da5600fbb3487069fb11d1947a4c8c2a12b9
Oleksii Moisieiev <oleksii_moisieiev@epam.com>
2021-02-19 18:34:39 +0200

    fixup! [iamanager] Add dirty hack mirroring certificates and fix script path
    Signed-off-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>

--- recipes-domd/domd-image-minimal/files/meta-xt-prod-extra/recipes-aos/aos-iamanager/files/finish.sh
+++ recipes-domd/domd-image-minimal/files/meta-xt-prod-extra/recipes-aos/aos-iamanager/files/finish.sh
@@ -3,7 +3,10 @@
 #TBD Dirty hack. Should be removed
 scp -r -o "ServerAliveInterval=1" -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" /var/aos/crypt/*  root@192.168.0.3:/var/aos/crypt/
 
-systemctl restart aos.target
 
 # reboot domf
 xenstore-write control/user-reboot 2
+
+sleep 3
+
+systemctl restart aos.target
