56e0c483e01e4e8cc6805bb8139b575a26d4dd29
Yevgen Abramov <Yevgen_Abramov@epam.com>
2021-02-25 15:18:11 +0200

    fixup! [iamanager] Add dirty hack mirroring certificates and fix script path

--- recipes-domd/domd-image-minimal/files/meta-xt-prod-extra/recipes-aos/aos-iamanager/files/finish.sh
+++ recipes-domd/domd-image-minimal/files/meta-xt-prod-extra/recipes-aos/aos-iamanager/files/finish.sh
@@ -1,7 +1,9 @@
 # !/bin/sh
 
 #TBD Dirty hack. Should be removed
+ssh -o "ServerAliveInterval=1" -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" root@192.168.0.3 'mkdir -p /var/aos/crypt; sync'
 scp -r -o "ServerAliveInterval=1" -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" /var/aos/crypt/*  root@192.168.0.3:/var/aos/crypt/
+ssh -o "ServerAliveInterval=1" -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" root@192.168.0.3 'sync'
 
 sync
 
